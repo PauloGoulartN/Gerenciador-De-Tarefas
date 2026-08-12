9c568bca665d429e01061ab10369e600e6366d62
Paulo <baixamemoria97@gmail.com>
2026-08-12 16:12:46 -0300

    feat: implementar estado das tarefas e exclusão de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
+// ARMAZENAR STATE TASK
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      tittle: "Estudar React",
+      description: "Estudar React para aprender a criar aplicações web",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      tittle: "Estudar Node.js",
+      description: "Estudar Node.js para aprender a criar aplicações web",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      tittle: "Estudar postgreSQL",
+      description: "Estudar postgreSQL para aprender a criar aplicações web",
+      isCompleted: false,
+    },
+  ]);
+
+  // FUNÇÃO PARA ALTERAR O ESTADO DA TASK
+  function onTaskClick(taskId) {
+    const newTask = tasks.map((task) => {
+      // se a task.id for igual ao taskId, ele vai retornar uma nova task com o isCompleted invertido
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      // equivalente a um else, caso a condição não seja satisfeita, ele vai retornra a task original
+      return task;
+    });
+    setTasks(newTask);
+  }
+
+  //FUNÇÃO PARA DELETAR TASK
+  function deleteTaskOnclick(taskId) {
+    const newTask = tasks.filter((task) => task.id !==  taskId);
+    setTasks(newTask);
+  }
+
   return (
-    <div>
-      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
-      <AddTask />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} deleteTaskOnClick={deleteTaskOnclick} />
+      </div>
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,32 @@
-function Tasks() {
-  return <h1>Tasks</h1>;
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+
+function Tasks({tasks, onTaskClick, deleteTaskOnClick}) {
+  return (
+    <ul className="space-y-2 p-6 bg-slate-200 rounded-md shadow">
+      {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => onTaskClick(task.id)}
+            className={
+              "bg-slate-400 text-left w-full text-white p-2 rounded-md " +
+              (task.isCompleted && "line-through")
+            }
+          >
+            {task.tittle}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => deleteTaskOnClick(task.id)} // Adicionei a função de deletar task
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
+            <TrashIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;
